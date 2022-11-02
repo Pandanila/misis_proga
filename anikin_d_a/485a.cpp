c90718ef57247490bc1e08f9350307c3096b232f
Pandanila <97802843+Pandanila@users.noreply.github.com>
2022-11-03 00:51:07 +0400

    Update 485a.cpp

--- anikin_d_a/485a.cpp
+++ anikin_d_a/485a.cpp
@@ -1,1 +1,34 @@
-решать через зацикливание остатков
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+int main() {
+	int a = 0;
+	int m = 0;
+	int rem = 0;
+	int flag = 1;
+
+	std::cin >> a >> m;
+
+	std::set<int> remains;
+	rem = a % m;
+
+	while (remains.count(rem) != 1) {
+		a += rem;
+		remains.insert(rem);
+
+		if (a % m == 0) {
+			flag = 0;
+			break;
+		}
+		rem = a % m;
+	}
+
+	if (flag) {
+		std::cout << "No";
+	}
+	else {
+		std::cout << "Yes";
+	}
+}
