cf9bcd9c1f0ad3076f66476900f43477514c4c93
Pandanila <97802843+Pandanila@users.noreply.github.com>
2022-11-08 14:23:00 +0400

    Update 1165b.cpp

--- anikin_d_a/1165b.cpp
+++ anikin_d_a/1165b.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <set>
 
-int main(){
-	std::vector<int> nums;
+int main() {
 	int n = 0;
+
 	std::cin >> n;
+	std::multiset<int> s;
+
 	for (int i = 0; i < n; i += 1) {
 		int a = 0;
 		std::cin >> a;
-		nums.push_back(a);
+		s.insert(a);
 	}
 
+	int count = 0;
+	int day = 1;
 
+	std::set<int>::iterator it = s.begin();
 
-	std::cout << nums.size();
+	while (it != s.end()) {
+		if (*it >= day) {
+			day += 1;
+			count += 1;
+		}
+		it++;
+	}
+
+	std::cout << count;
 }
-
-решить с помощью сортирвоки массива
