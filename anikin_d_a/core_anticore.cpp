580ff1bbd3d62870c2700c847961a40943008047
Pandanila <97802843+Pandanila@users.noreply.github.com>
2022-12-06 00:48:21 +0400

    Create core_anticore.cpp

---
+++ anikin_d_a/core_anticore.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <cmath>
+
+struct Rdec2D {
+	double x = 0;
+	double y = 0;
+};
+
+struct Rpol2D {
+	double r = 0;
+	double phi = 0;
+};
+
+std::ostream& operator<<(std::ostream &out, const Rpol2D &point) {
+	out << "(" << point.r << "," << point.phi << ")" << "\n";
+	return out;
+}
+
+Rdec2D ToDec(Rpol2D vector) {
+	double x = std::cos(vector.phi) * vector.r;
+	double y = std::sin(vector.phi) * vector.r;
+	Rdec2D new_vector{ x, y };
+	return new_vector;
+}
+
+Rpol2D ToPol(Rdec2D vector) {
+	double r = std::sqrt(vector.x * vector.x + vector.y * vector.y);
+	double phi = std::asin(vector.y / r);
+	Rpol2D new_vector{ r, phi };
+	return new_vector;
+}
+
+std::ostream& operator<<(std::ostream &out, const Rdec2D &point) {
+	out << "(" << point.x << "," << point.y << ")" << "\n";
+	return out;
+}
+
+Rdec2D operator+=(Rdec2D& lhs, const Rdec2D& rhs) {
+	lhs.x += rhs.x;
+	lhs.y += rhs.y;
+	return lhs;
+}
+
+Rdec2D operator-=(Rdec2D& lhs, const Rdec2D& rhs) {
+	lhs.x -= rhs.x;
+	lhs.y -= rhs.y;
+	return lhs;
+}
+
+Rdec2D operator+(const Rdec2D& lhs, const Rdec2D& rhs) {
+	Rdec2D res = lhs;
+	res += rhs;
+	return res;
+}
+
+Rdec2D operator-(const Rdec2D& lhs, const Rdec2D& rhs) {
+	Rdec2D res = lhs;
+	res -= rhs;
+	return res;
+}
+
+Rdec2D operator*=(Rdec2D& lhs, const int& rhs) {
+	lhs.x *= rhs;
+	lhs.y *= rhs;
+	return lhs;
+}
+
+Rdec2D operator*(Rdec2D& lhs, const int& rhs) {
+	Rdec2D& res = lhs;
+	res *= rhs;
+	return res;
+}
+
+double norm(const Rdec2D& vec) {
+	return std::sqrt(vec.x * vec.x + vec.y * vec.y);
+}
+
+double dot(const Rdec2D& lhs, const Rdec2D& rhs) {
+	return (lhs.x*rhs.x + lhs.y*rhs.y);
+}
+
+Rpol2D operator+=(Rpol2D lhs, Rpol2D rhs) {
+	Rdec2D new_lhs = ToDec(lhs);
+	Rdec2D new_rhs = ToDec(rhs);
+	new_lhs += new_rhs;
+	lhs = ToPol(new_lhs);
+	std::cout << lhs << "\n";
+	return lhs;
+}
+//модификация № 1. Когда неизвестна первоначальная скорость противоядра
+int main() {
+	Rdec2D core_rad_vector_1 = { 0, 6 };
+	Rdec2D core_velocity_1 = { 1000, 1000 };
+	Rdec2D core_velocity_x_1 = { core_velocity_1.x, 0 };
+	Rdec2D core_velocity_y_1 = { 0, core_velocity_1.y };
+	Rdec2D core_rad_vector_2 = { 200, 6 };
+	Rdec2D core_velocity_2 = { 0, 0 };
+	Rdec2D g = { 0, -9.8 };
+	double s = norm(core_rad_vector_1 - core_rad_vector_2);
+	double t_1 = 0;
+	double delta_t = 1;
+
+	//вычисляем время, за которое звук достигнет точки запуска противоядра
+	double sound_t = s / 331;
+
+	while (t_1 < sound_t) {
+		t_1 += delta_t;
+		core_velocity_y_1 += g;
+		core_velocity_1 = core_velocity_x_1 + core_velocity_y_1;
+		core_rad_vector_1 += core_velocity_1;
+		if (t_1 >= sound_t) {
+			core_velocity_y_1 += g;
+			core_velocity_1 = core_velocity_x_1 + core_velocity_y_1;
+			core_rad_vector_1 += core_velocity_1;
+			core_velocity_2 = core_rad_vector_1 - core_rad_vector_2;
+			std::cout << core_velocity_2;
+			break;
+		}
+	}
+}
