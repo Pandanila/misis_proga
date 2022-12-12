1e0068b7376c8c6cf606465d79546edc015ef220
Pandanila <97802843+Pandanila@users.noreply.github.com>
2022-12-13 03:41:01 +0400

    Update main.cpp

--- anikin_d_a/ProblemWithPVO/main.cpp
+++ anikin_d_a/ProblemWithPVO/main.cpp
@@ -46,21 +46,10 @@ int collision_of_cores(Rdec2D r_1, Rdec2D v_1_x, Rdec2D v_1_y,
     return 0;
 }
 
-Rdec2D coords_of_core(Rdec2D r, Rdec2D v, Rdec2D g){
-    std::vector<double> coords;
-    Rdec2D v_x = {v.x, 0};
-    Rdec2D v_y = {0, v.y};
-    v = v_x + v_y + g;
-    r += v;
-    coords.push_back(r.x);
-    coords.push_back(r.y);
-    return r;
-}
-
 int main() {
     Rdec2D r_1 = { 0, 6 };
     Rdec2D v_1 = { 10, 1000 };
-    Rdec2D r_2 = { 200, 6 };
+    Rdec2D r_2 = { 201, 6 };
     Rdec2D v_2 = {0, 0};
     Rdec2D g = {0, -9.8};
     double s = norm(r_1 - r_2);
@@ -68,43 +57,46 @@ int main() {
     double t = 0;
     double delta_t = 1;   //точность изменения скорости
     double delta_v = 10;  //точность подбор скорости для противоядра
-    double delta_s = 0;   //точность координаты при столкновении
+    double delta_s = 11;   //точность координаты при столкновении
 
     std::vector<Rdec2D> list_of_v;
 
-    for (double x = -3000; x < 3001; x += delta_v){
-        for (double y = 0; y < 3001; y += delta_v){
+    for (double x = -500; x < 500; x += delta_v){
+        for (double y = 0; y < 1000; y += delta_v){
             int k = collision_of_cores(r_1, {v_1.x, 0}, {0, v_1.y}, r_2, {x, 0}, {0, y}, delta_t, delta_s);
+            //std::cout << k;
 
             if (k == 1) {
-                Rdec2D a = {x, 0};
-                Rdec2D b = {0, y};
-                list_of_v.push_back(a + b);
+                Rdec2D a = {x, y};
+                list_of_v.push_back(a);
+                std::cout << a;
             }
         }
     }
 
     std::ofstream file;
-    file.open("coords.csv");
+    file.open("coords1.csv");
 
 
     for (int i = 0; i < list_of_v.size(); i += 1){
         r_1 = { 0, 6 };
         v_1 = { 10, 1000 };
-        r_2 = { 200, 6 };
+        r_2 = { 500, 6 };
         v_2 = {0, 0};
         t = 0;
         v_2 = list_of_v[i];
         file << v_2 << "\n";
         file << r_1.x << "\t" << r_1.y << " \t" << r_2.x << "\t" << r_2.y << "\t" << t << "\t" << s << "\n";
-        while (std::abs(norm(r_1 - r_2)) > delta_s) {
+        while (std::abs(norm(r_1 - r_2)) > delta_s && r_1.y >= 0 && r_2.y >= 0) {
             t += delta_t;
-            r_1 = coords_of_core(r_1, v_1, g);
+            v_1 += g;
+            r_1 += v_1;
 
             if (t >= t_of_sound) {
-                r_2 = coords_of_core(r_2, v_2, g);
+                v_2 += g;
+                r_2 += v_2;
             }
-            std::cout << r_1.x << "\t" << r_1.y << " \t" << r_2.x << "\t" << r_2.y << "\n";
+            //std::cout << r_1.x << "\t" << r_1.y << " \t" << r_2.x << "\t" << r_2.y << "\n";
             file << r_1.x << "\t" << r_1.y << " \t" << r_2.x << "\t" << r_2.y << "\t" << t << "\t" << s << "\n";
         }
     }
