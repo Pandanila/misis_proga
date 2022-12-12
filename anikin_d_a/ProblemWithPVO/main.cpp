987ee3608180164c67b7c084c71de13929378c66
Pandanila <97802843+Pandanila@users.noreply.github.com>
2022-12-13 01:55:09 +0400

    Update main.cpp

--- anikin_d_a/ProblemWithPVO/main.cpp
+++ anikin_d_a/ProblemWithPVO/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
-
-#include <iostream>
-#include <cmath>
-
 #include <geom.hpp>
+#include <vector>
 
 int collision_of_cores(Rdec2D r_1, Rdec2D v_1_x, Rdec2D v_1_y,
                        Rdec2D r_2, Rdec2D v_2_x, Rdec2D v_2_y,
@@ -38,45 +35,79 @@ int collision_of_cores(Rdec2D r_1, Rdec2D v_1_x, Rdec2D v_1_y,
         }
 
         if (r_2.x < r_1.x){
-            if (r_2.y < r_1.y){
-                return 2; // недолёт
+            if (r_2.y < r_1.y){ //недолет
+                return 2;
             }
-            if (r_2.y > r_1.y){
-                return 3; //перелет
+            if (r_2.y > r_1.y){  //перелет
+                return 3;
             }
         }
     }
     return 0;
 }
 
+Rdec2D coords_of_core(Rdec2D r, Rdec2D v, Rdec2D g){
+    std::vector<double> coords;
+    Rdec2D v_x = {v.x, 0};
+    Rdec2D v_y = {0, v.y};
+    v = v_x + v_y + g;
+    r += v;
+    coords.push_back(r.x);
+    coords.push_back(r.y);
+    return r;
+}
+
 int main() {
-    Rdec2D core_rad_vector_1 = { 0, 6 };
-    Rdec2D core_velocity_1 = { 10, 1000 };
-    Rdec2D core_velocity_x_1 = { core_velocity_1.x, 0 };
-    Rdec2D core_velocity_y_1 = { 0, core_velocity_1.y };
-    Rdec2D core_rad_vector_2 = { 200, 6 };
-    Rdec2D core_velocity_2 = { -10, 2000 };
-    Rdec2D core_velocity_x_2 = { core_velocity_2.x, 0 };
-    Rdec2D core_velocity_y_2 = { 0, core_velocity_2.y };
-    Rdec2D g = { 0, -9.8 };
-    double s = norm(core_rad_vector_1 - core_rad_vector_2);
-    double t_1 = 0;
-    double delta_t = 1;
-    double delta_v = 10;
+    Rdec2D r_1 = { 0, 6 };
+    Rdec2D v_1 = { 10, 1000 };
+    Rdec2D r_2 = { 200, 6 };
+    Rdec2D v_2 = {0, 0};
+    Rdec2D g = {0, -9.8};
+    double s = norm(r_1 - r_2);
+    double t_of_sound = s / 300;
+    double t = 0;
+    double delta_t = 1;   //точность изменения скорости
+    double delta_v = 10;  //точность подбор скорости для противоядра
+    double delta_s = 0;   //точность координаты при столкновении
+
+    std::vector<Rdec2D> list_of_v;
 
     for (double x = -3000; x < 3001; x += delta_v){
         for (double y = 0; y < 3001; y += delta_v){
-            int k = collision_of_cores(core_rad_vector_1, core_velocity_x_1, core_velocity_y_1,
-                                       core_rad_vector_2, {x, 0}, {0, y},
-                                       1, 0);
+            int k = collision_of_cores(r_1, {v_1.x, 0}, {0, v_1.y}, r_2, {x, 0}, {0, y}, delta_t, delta_s);
 
             if (k == 1) {
                 Rdec2D a = {x, 0};
                 Rdec2D b = {0, y};
-                std::cout << a + b;
+                list_of_v.push_back(a + b);
             }
         }
     }
 
+    std::ofstream file;
+    file.open("coords.csv");
 
+
+    for (int i = 0; i < list_of_v.size(); i += 1){
+        r_1 = { 0, 6 };
+        v_1 = { 10, 1000 };
+        r_2 = { 200, 6 };
+        v_2 = {0, 0};
+        t = 0;
+        v_2 = list_of_v[i];
+        file << v_2 << "\n";
+        file << r_1.x << "\t" << r_1.y << " \t" << r_2.x << "\t" << r_2.y << "\t" << t << "\t" << s << "\n";
+        while (std::abs(norm(r_1 - r_2)) > delta_s) {
+            t += delta_t;
+            r_1 = coords_of_core(r_1, v_1, g);
+
+            if (t >= t_of_sound) {
+                r_2 = coords_of_core(r_2, v_2, g);
+            }
+            std::cout << r_1.x << "\t" << r_1.y << " \t" << r_2.x << "\t" << r_2.y << "\n";
+            file << r_1.x << "\t" << r_1.y << " \t" << r_2.x << "\t" << r_2.y << "\t" << t << "\t" << s << "\n";
+        }
+    }
+
+    file.close();
 }
